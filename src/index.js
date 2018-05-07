a7d297e54c0e2e1e28380eda5fcad85e1664978a
lilei <289670502@qq.com>
2018-05-07 15:01:09 +0800

    opt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import {RayrModal} from 'react-rayr-modal';
 import {RayrBtn, RayrBtnGroup} from 'react-rayr-btn';
+import {RayrImgview} from 'react-rayr-imgview';
+import {RayrIcon} from 'react-rayr-icon';
 
 import RayrComponent from './RayrComponent.js';
 
-export {RayrComponent, RayrModal, RayrBtn, RayrBtnGroup};
+export {RayrComponent, RayrModal, RayrBtn, RayrBtnGroup, RayrImgview, RayrIcon};
