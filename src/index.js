ae427bd6dbdd527e2e99e2567dd1f2c24531113b
hongfeng <hongfeng@didichuxing.com>
2018-05-07 18:43:36 +0800

    引入pagination

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import {RayrModal} from 'react-rayr-modal';
 import {RayrBtn, RayrBtnGroup} from 'react-rayr-btn';
 import {RayrImgview} from 'react-rayr-imgview';
 import {RayrIcon} from 'react-rayr-icon';
+import {RayrPagination} from 'react-rayr-pagination';
 
 import RayrComponent from './RayrComponent.js';
 
-export {RayrComponent, RayrModal, RayrBtn, RayrBtnGroup, RayrImgview, RayrIcon};
+export {RayrComponent, RayrModal, RayrBtn, RayrBtnGroup, RayrImgview, RayrIcon, RayrPagination};
