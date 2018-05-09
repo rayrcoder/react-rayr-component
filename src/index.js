8b5a3804904ddf06e5623990a3ac66bf7b4f101e
luokaihui <luokaihui@didichuxing.com>
2018-05-09 15:06:15 +0800

    加入RayrSelector

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import {RayrBtn, RayrBtnGroup} from 'react-rayr-btn';
 import {RayrImgview} from 'react-rayr-imgview';
 import {RayrIcon} from 'react-rayr-icon';
 import {RayrPagination} from 'react-rayr-pagination';
+import {RayrSelector} from 'react-rayr-dropdown';
+
 
 import RayrComponent from './RayrComponent.js';
 
-export {RayrComponent, RayrModal, RayrBtn, RayrBtnGroup, RayrImgview, RayrIcon, RayrPagination};
+export {RayrComponent, RayrModal, RayrBtn, RayrBtnGroup, RayrImgview, RayrIcon, RayrPagination, RayrSelector};
